ed384195752eca40d8f00d491b8c4ea36751956f
Jyunmau <yuanmou8@gmail.com>
2020-03-09 22:08:40 +0800

    refactor: 定下架构

--- core/feature_processing/feature_interface.py
+++ core/feature_processing/feature_interface.py
@@ -1,8 +1,8 @@
-# todo：这个文件完成特征提取器接口
 import abc
+import numpy as np
 
 
 class FeatureInterface(metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def get_feature(self):
+    def get_feature(self, image: np.ndarray):
         pass
